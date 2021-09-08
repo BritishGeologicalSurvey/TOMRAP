33d007a75dbee9e7567f9d5234f3d13ac2893964
Declan Valters <decval@bgs.ac.uk>
2021-09-08 11:04:14 +0100

    Update variable names and put in config

--- hazardmaps/config.py
+++ hazardmaps/config.py
@@ -1,21 +1,19 @@
 # CONFIG FILE
 
-DATADIR = "path"
-weightings = ["foo", "bar"]
 
-# etc...
+weightings = ["foo", "bar"]
 
 DATADIR = "../datadir/"
 # The one above is not found in the data folder - DV added on Friday - was in the other folder
-#expofile = DATADIR + "TZA_buildings_exposure_20200731.dbf" #contains location id and positions  - present
-expofile = DATADIR + "TZA_buildings_exposure_20200224.dbf" #contains location id and positions  - present
-expobfile = DATADIR + "TZA_buildings_exposure_breakdown_20200731.dbf" 
+#exposure_file = DATADIR + "TZA_buildings_exposure_20200731.dbf" #contains location id and positions  - present
+exposure_file = DATADIR + "TZA_buildings_exposure_20200224.dbf" #contains location id and positions  - present
+exposure_breakdown_file = DATADIR + "TZA_buildings_exposure_breakdown_20200731.dbf" 
 #contains location id and breakdown of number of each house type
-#volcP = ["kyejo", "meru"]
-#volcL = ["lengai", "ngozi", "rungwe"]
 volcfile = DATADIR + "World_Volcanoes_Smithsonian_Institution_GVP.shp" #point locations of volcanoes
 volcnames = ["Lengai, Ol Doinyo", "Meru", "Ngozi", "Rungwe", "Kyejo"] #names of volcanoes in Smithsonian shp
 
 # 1 in 100, 1 in 200 in the file
 floodratio = 100 # selects from different flood tifs
 floodtypes = ["FD", "FU", "P"]  #selects from different flood tifs
+# in the Seismic folder
+earthquake_file = DATADIR + "hazard_map_mean_tanzania.dbf" #contains earthquake information
