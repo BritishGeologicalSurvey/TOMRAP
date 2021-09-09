12575424e08b0e02cdd94707c838229cbf143800
Declan Valters <decval@bgs.ac.uk>
2021-09-09 17:57:09 +0100

    Fix typos

--- hazardmaps/test.py
+++ hazardmaps/test.py
@@ -7,3 +7,10 @@ def test_succeeds():
     ax = fig.add_subplot(1,1,1)
     ax.plot([1,2,3], linestyle='-')
     return fig
+
+
+"""
+To run the full test suite, you need to run the data ingest 
+script, and then call the plot_() function on the images
+you want to check (it will take a while)
+"""
