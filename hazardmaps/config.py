5f6bc31f27c968c9b74c6fc8c25cee0f375a7f76
Declan Valters <decval@bgs.ac.uk>
2021-09-29 18:59:00 +0100

    Generalise for the Nepal case, if possible

--- hazardmaps/config.py
+++ hazardmaps/config.py
@@ -1,11 +1,16 @@
 # CONFIG FILE
 
+SEISMIC = True
+FLOODING = True 
+VOLCANIC = False
+LANDSLIDE = False
+EXPOSURE = True
 
-DATADIR = "../datadir/"
+DATADIR = "../datadir/Nepal/"
 # The one above is not found in the data folder - DV added on Friday - was in the other folder
 #config.exposure_file = config.DATADIR + "TZA_buildings_exposure_20200731.dbf" #contains location id and positions  - present
-exposure_file = DATADIR + "TZA_buildings_exposure_20200224.dbf" #contains location id and positions  - present
-exposure_breakdown_file = DATADIR + "TZA_buildings_exposure_breakdown_20200731.dbf" 
+exposure_file = DATADIR + "NPL_buildings_exposure_20200214.dbf" #contains location id and positions  - present
+exposure_breakdown_file = DATADIR + "NPL_buildings_exposure_breakdown_20200214.dbf" 
 #contains location id and breakdown of number of each house type
 #volcP = ["kyejo", "meru"]
 #volcL = ["lengai", "ngozi", "rungwe"]
@@ -16,13 +21,15 @@ volcnames = ["Lengai, Ol Doinyo", "Meru", "Ngozi", "Rungwe", "Kyejo"] #names of 
 floodratio = 100 # selects from different flood tifs
 floodtypes = ["FD", "FU", "P"]  #selects from different flood tifs
 # in the Seismic folder
-eearthquake_file = DATADIR + "hazard_map_mean_tanzania.dbf" #contains earthquake information
+eearthquake_file = DATADIR + "hazard_map_mean_nepal.dbf" #contains earthquake information
 
 figure_prefix = "output_"
 #plot_types = ["ear", "plu", "flu", "tep", "lahar", "pgaindx", "P", "FU", "lah", "pyr", "equ", "flood", "volc", "hmap"]
 # OVERWRITE QUICK TEST
 plot_types = "hmap"
 
+# TANZANIA
+"""
 building_type_tz = ['CR/LFM/HBET:1,3',
                     'CR/LFM/HBET:4,7',
                     'CR/LFM/HBET:8,20',
@@ -38,5 +45,12 @@ building_type_tz = ['CR/LFM/HBET:1,3',
                     'MUR+CL99', 
                     'MUR+STRUB',
                      'W+WWD']
+"""
 # Numbers could be no. of floors
 # Could be made more general. Could be different in UK for e.g.
+#  NEPAL
+building_type_tz = ['C99/LFINF+DNO/HBET:1,3', 'C99/LFINF+DNO/HBET:4,7',
+       'C99/LFINF+DNO/HBET:8,20', 'MATO/LN', 'MUR+ADO/HBET:1,3',
+       'MUR+CL99+MOC', 'MUR+CL99+MOM', 'MUR+CL99/HBET:1,3',
+       'MUR+CL99/HBET:4,7', 'MUR+STRUB+MOL', 'MUR+STRUB+MOM', 'S', 'S/LFINF',
+       'W', 'W+WWD']
